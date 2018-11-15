7abc84ba51412f6dbc546d0151f8047aaacb8e1f
legionld <38010214+legionld@users.noreply.github.com>
2018-11-15 18:34:30 +0300

    Added includes files

---
+++ includes/creation.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   creation.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschille <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 18:49:12 by jschille          #+#    #+#             */
+/*   Updated: 2018/11/07 21:25:31 by jschille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CREATION_H
+# define CREATION_H
+
+t_point	*create_point(int x, int y);
+t_sqrt	*create_sqrt(int x, int y, int px, int py);
+#endif
